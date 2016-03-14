504dabba49e33bbb085f0e5488c2f3a50837b957
sparklyballs <sparklyballs@gmail.com>
2016-03-14 15:28:19 +0000

    match config init from mariadb

--- init/30_config.sh
+++ init/30_config.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-if [ ! -d "/config/log/myqsl" ]; then
-mkdir -p /config/log/mysql
-fi
+# make folders if required
+mkdir -p "$DATADIR" /var/run/mysqld /config/log/mysql
 
+# configure my.cnf and mysqld_safe
+sed -i 's/key_buffer\b/key_buffer_size/g' /etc/mysql/my.cnf
+sed -ri 's/^(bind-address|skip-networking)/;\1/' /etc/mysql/my.cnf
+sed -i s#/var/log/mysql#/config/log/mysql#g /etc/mysql/my.cnf
+sed -i -e 's/\(user.*=\).*/\1 abc/g' /etc/mysql/my.cnf
+sed -i -e "s#\(datadir.*=\).*#\1 $DATADIR#g" /etc/mysql/my.cnf
+sed -i "s/user='mysql'/user='abc'/g" /usr/bin/mysqld_safe
 
-if [ ! -d "$DATADIR" ]; then
-mkdir -p "$DATADIR"
-fi
+# setup custom cnf file
+[[ ! -f /config/custom.cnf ]] && cp /defaults/my.cnf /config/custom.cnf
+[[ ! -L /etc/mysql/conf.d/custom.cnf && -f /etc/mysql/conf.d/custom.cnf ]] && rm /etc/mysql/conf.d/custom.cnf
+[[ ! -L /etc/mysql/conf.d/custom.cnf ]] && ln -s /config/custom.cnf /etc/mysql/conf.d/custom.cnf
 
-if [ ! -d "/var/run/mysqld" ]; then
-mkdir -p /var/run/mysqld
+# set permissions
 chmod -R 777 /var/run/mysqld
-fi
+
